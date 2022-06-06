d92833d06e6019dd3bc57ff3f860fb0605c9882e
byht1 <99541944+byht1@users.noreply.github.com>
2022-06-06 08:49:07 +0000

    Deploying to gh-pages from @ VladaBoiko/helato@f582b8d26db8f3d8c4069f82d8af7af789f328fb 🚀

---
+++ index.e84ffd6b.js
@@ -0,0 +1,2 @@
+$(".single-item").slick(),$(".one-time").slick({arrows:!1,dots:!0,infinite:!0,speed:300,slidesToShow:1,adaptiveHeight:!0});
+//# sourceMappingURL=index.e84ffd6b.js.map
--- index.f28b0012.js
+++
@@ -1,2 +0,0 @@
-$(".single-item").slick(),$(".one-time").slick({dots:!0,infinite:!0,speed:300,slidesToShow:1,adaptiveHeight:!0});
-//# sourceMappingURL=index.f28b0012.js.map

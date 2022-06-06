f582b8d26db8f3d8c4069f82d8af7af789f328fb
byht1 <vitalikbyht1@gmail.com>
2022-06-06 11:47:50 +0300

    fix

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,8 +1,9 @@
 $('.single-item').slick();
 $('.one-time').slick({
+  arrows: false,
   dots: true,
   infinite: true,
   speed: 300,
   slidesToShow: 1,
-  adaptiveHeight: true
+  adaptiveHeight: true,
 });
